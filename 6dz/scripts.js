441f9ec95e67ff66e32c467b781cd8b63bc0b61f
Olga Kuzmenko <germioneys@gmail.com>
2020-09-24 19:48:26 +0300

    new homework

--- 6dz/scripts.js
+++ 6dz/scripts.js
@@ -31,7 +31,7 @@ let result = {
     elements: numbersArray.concat(stringsArray).length,
     minInteger: Math.min(...integersArray),
     maxInteger: Math.max(...integersArray),
-    arithmeticMean: numbersArray.reduce(function(sum, current) {return sum + current;}, 0) / numbersArray.length,
+    arithmeticMean: integersArray.reduce(function(sum, current) {return sum + current;}, 0) / numbersArray.length,
     evenPositiveIntegerElements: integersArray.filter((value) => {if(value > 0 && value % 1 === 0 && value % 2 === 0) {return value;} }).length,
     negativeElements: numbersArray.filter((value) => {if(value < 0){return value;}}).length,
     fractionSum: floatArray.reduce(function(sum, current) {return sum + current;}, 0),
