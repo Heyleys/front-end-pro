e1645146c49037eec8194eef4a4d36aa03a48e79
Olga Kuzmenko <germioneys@gmail.com>
2020-11-03 01:45:05 +0200

    edit homework

--- 16dz/scripts.js
+++ 16dz/scripts.js
@@ -1,21 +1,19 @@
 'use strict'
 
-const hours = document.querySelector('.hours');
-const minutes = document.querySelector('.minutes');
-const seconds = document.querySelector('.seconds');
+function setDate(initTime) {
+    const hours = document.querySelector('.hours');
+    const minutes = document.querySelector('.minutes');
+    const seconds = document.querySelector('.seconds');
 
-function setDate() {
-    const now = new Date();
+    const now = new Date(initTime);
     const secs = now.getSeconds();
     const mins = now.getMinutes();
     const hrs = now.getHours();
 
-    hours.innerHTML = hrs;
-
-    if (secs < 10) {
-        seconds.innerHTML = '0' + secs;
+    if (hrs < 10) {
+        hours.innerHTML = '0' + hrs;
     } else {
-        seconds.innerHTML = secs;
+        hours.innerHTML = hrs;
     }
     
     if (mins < 10) {
@@ -23,6 +21,26 @@ function setDate() {
     } else {
         minutes.innerHTML = mins;
     }
+
+    if (secs < 10) {
+        seconds.innerHTML = '0' + secs;
+    } else {
+        seconds.innerHTML = secs;
+    }
 }
 
-setInterval(setDate,1000);
+function runPresetupedClock(initTime = null){
+    if(initTime === null){
+        initTime = new Date();
+    }
+    initTime = Date.parse(initTime);
+    let timeIncrementStep = 1000;
+    setDate(initTime);
+    setInterval(function(){
+        initTime += timeIncrementStep;
+        setDate(initTime);
+    },1000);
+}
+
+//You can input any hardcoded starting datetime or not use a param for using current timestamp as default param
+runPresetupedClock();
